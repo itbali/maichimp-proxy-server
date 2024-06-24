4e1c5ca4f28c77c6bf72231d629122048e4c0f92
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-06-24 19:45:12 +0200

    3

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ const dotenv = require('dotenv');
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
 const app = express();
-app.use(cors());
+app.use(cors((origin, callback)=>{
+    if (origin.includes("vercel.app")) {
+        callback(null, true);
+    } else {
+        callback(new Error('Not allowed by CORS'));
+    }
+}));
 const apiProxy = createProxyMiddleware('/api', {
     target: 'https://us7.api.mailchimp.com/3.0',
     changeOrigin: true,
     pathRewrite: {
         '^/api': '',
     },
-    onProxyReq: (proxyReq, req, res) => {
+    onProxyReq: (proxyReq) => {
         console.log('proxyReq to '+process.env.MAILCHIMP_KEY, proxyReq);
         proxyReq.setHeader('Authorization', `Basic ${Buffer.from(`anystring:${process.env.MAILCHIMP_KEY}`).toString('base64')}`);
     }
